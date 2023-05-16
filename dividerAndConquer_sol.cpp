208e904c59859c3b533ab0a5e76502de058f6668
Ahmed Mahmoud <104006521+AhMmedMahmoud@users.noreply.github.com>
2023-05-16 20:55:29 +0300

    Update dividerAndConquer_sol.cpp

--- dividerAndConquer_sol.cpp
+++ dividerAndConquer_sol.cpp
@@ -5,17 +5,18 @@ using namespace std;
 
 pair<int, vector<int>> solution(int no_of_switches, int n, bool reversed)
 {
+	//------------------------------sefety codition--------------------------------------------
 	if (n == 0 || n < 1){
 	    vector<int> vec = { };
 	    return make_pair(0, vec);
     	}
-    
+        //------------------------------------base case---------------------------------------------
 	if (n == 1){
 	    cout << no_of_switches - 1 << "\t";
 	    vector<int> vec = { no_of_switches - 1 };
 	    return make_pair(1, vec);
 	}
-
+        //------------------------------------base case---------------------------------------------
 	if (n == 2 && !reversed){
 	    cout << no_of_switches - 2 << "\t" << no_of_switches - 1 << "\t";
 	    vector<int> vec = { no_of_switches - 2,no_of_switches - 1 };
@@ -30,23 +31,19 @@ pair<int, vector<int>> solution(int no_of_switches, int n, bool reversed)
 
 	if (!reversed){
 	    int sum = 0;
-	    //--------------------- turn off n-2 switches from right 
+	    //----------------------------turn off n-2 switches from right-------------------------------
 	    pair<int, vector<int>> result1 = solution(no_of_switches, n - 2, reversed);
 	    sum = result1.first;
-
-	    //--------------------- turn off toggle right switch to n-2 switches from right
+	    //--------------------- turn off toggle right switch to n-2 switches from right----------------
 	    cout << no_of_switches - n << "\t";
 	    sum++;
-
-	    //--------------------- turn on n-2 switches from right 
+	    //--------------------- turn on n-2 switches from right----------------------------------------  
 	    pair<int, vector<int>> result2 = solution(no_of_switches, n - 2, !reversed);
 	    sum += result2.first;
-
-	    //---------------------- solve same problem for n-1 switches
+            //---------------------- solve same problem for n-1 switches------------------------------------
 	    pair<int, vector<int>> result3 = solution(no_of_switches, n - 1, reversed);
 	    sum += result3.first;
-
-	    //----------------------returning min number and sequece---------------------------------------
+            //----------------------returning min number and sequece---------------------------------------
 	    vector<int> vec;
 	    for (int i = 0; i < result1.second.size(); i++)
 	       vec.push_back(result1.second[i]);
@@ -59,18 +56,19 @@ pair<int, vector<int>> solution(int no_of_switches, int n, bool reversed)
 	}
 	else{
 	    int sum = 0;
+	    //---------------------- solve same problem for n-1 switches------------------------------------
 	    pair<int, vector<int>> result1 = solution(no_of_switches, n - 1, reversed);
 	    sum = result1.first;
-		
+	    //--------------------- turn on n-2 switches from right----------------------------------------  
             pair<int, vector<int>> result2 =  solution(no_of_switches, n - 2, !reversed);
             sum += result2.first;
-        
+            //--------------------- turn on n-2 switches from right---------------------------------------- 
 	    cout << no_of_switches - n << "\t";
 	    sum++;
-	
+	    //----------------------------turn off n-2 switches from right-------------------------------
 	    pair<int, vector<int>> result3 = solution(no_of_switches, n - 2, reversed);
 	    sum += result3.first;
-
+	    //----------------------returning min number and sequece---------------------------------------
 	    vector<int> vec;
 	    for (int i = 0; i < result1.second.size(); i++)
     	      vec.push_back(result1.second[i]);
@@ -82,7 +80,6 @@ pair<int, vector<int>> solution(int no_of_switches, int n, bool reversed)
     	    return make_pair(sum, vec);
 	}
 }
-
 int main()
 {
     int no_of_switches = 5;
