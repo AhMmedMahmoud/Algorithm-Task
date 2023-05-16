731c43ffa5a00248048b5ae0a90d7bb0aaebcbb1
Ahmed Mahmoud <104006521+AhMmedMahmoud@users.noreply.github.com>
2023-05-16 20:46:37 +0300

    Update dividerAndConquer_sol.cpp

--- dividerAndConquer_sol.cpp
+++ dividerAndConquer_sol.cpp
@@ -1,138 +1,92 @@
 #include <iostream>
+#include <vector>
+#include <utility>
 using namespace std;
 
-int step = 0;
-
-void state(bool switches[], int no_of_switches)
+pair<int, vector<int>> solution(int no_of_switches, int n, bool reversed)
 {
-	cout << "step " << step << "    ";
-	for (int i = 0; i < no_of_switches; i++)
-	cout << switches[i] << "\t";
-	cout << endl;
-	step++;
-}
-void solution(bool switches[], int no_of_switches, int n, bool reversed)
-{
-	if (n == 0 || n < 1)
-	return;
-
-	if (n == 1)
-	{
-		cout << "n = 1"  << endl;
-		if (switches[no_of_switches - 1] != 0)
-		{
-			switches[no_of_switches - 1] = 0;  state(switches, no_of_switches);
-		}
-
-		return;
+	if (n == 0 || n < 1){
+	    vector<int> vec = { };
+	    return make_pair(0, vec);
+    	}
+    
+	if (n == 1){
+	    cout << no_of_switches - 1 << "\t";
+	    vector<int> vec = { no_of_switches - 1 };
+	    return make_pair(1, vec);
 	}
 
-	if (n == 2 && !reversed)
-	{
-		cout << "n = 2" << endl;
-		if (switches[no_of_switches - 2] != 1)
-		{
-			switches[no_of_switches - 2] = 1;    state(switches, no_of_switches);
-		}
-
-		if (switches[no_of_switches - 1] != 1)
-		{
-			switches[no_of_switches - 1] = 1;    state(switches, no_of_switches);
-		}
-
-		if (switches[no_of_switches - 2] != 0)
-		{
-			switches[no_of_switches - 2] = 0;     state(switches, no_of_switches);
-		}
-
-		if (switches[no_of_switches - 1] != 1)
-		{
-			switches[no_of_switches - 1] = 1;	  state(switches, no_of_switches);
-		}
-
-		if (switches[no_of_switches - 2] != 0)
-		{
-			switches[no_of_switches - 2] = 0;     state(switches, no_of_switches);
-		}
-
-		if (switches[no_of_switches - 1] != 0)
-		{
-			switches[no_of_switches - 1] = 0;    state(switches, no_of_switches);
-		}
-
-		return;
+	if (n == 2 && !reversed){
+	    cout << no_of_switches - 2 << "\t" << no_of_switches - 1 << "\t";
+	    vector<int> vec = { no_of_switches - 2,no_of_switches - 1 };
+	    return make_pair(2, vec);
 	}
 
-	if (n == 2 && reversed)
-	{
-		cout << "n = 2 reversed" << endl;
-		if (switches[no_of_switches - 2] != 0)
-		{
-			switches[no_of_switches - 2] = 0;     state(switches, no_of_switches);
-		}
-
-		if (switches[no_of_switches - 1] != 0)
-		{
-			switches[no_of_switches - 1] = 0;    state(switches, no_of_switches);
-		}
-
-		if (switches[no_of_switches - 2] != 0)
-		{
-			switches[no_of_switches - 2] = 0;     state(switches, no_of_switches);
-		}
-
-		if (switches[no_of_switches - 1] != 1)
-		{
-			switches[no_of_switches - 1] = 1;	  state(switches, no_of_switches);
-		}
-
-		if (switches[no_of_switches - 2] != 1)
-		{
-			switches[no_of_switches - 2] = 1;    state(switches, no_of_switches);
-		}
-
-		if (switches[no_of_switches - 1] != 1)
-		{
-			switches[no_of_switches - 1] = 1;    state(switches, no_of_switches);
-		}
-
-		return;
+	if (n == 2 && reversed){
+	    cout << no_of_switches - 1 << "\t" << no_of_switches - 2 << "\t";
+	    vector<int> vec = { no_of_switches - 1,no_of_switches - 2 };
+	    return make_pair(2, vec);
 	}
 
-	if (!reversed)
-	{
-		//--------------------- turn off n-2 switches from right
-		solution(switches, no_of_switches, n - 2, reversed);
+	if (!reversed){
+	    int sum = 0;
+	    //--------------------- turn off n-2 switches from right 
+	    pair<int, vector<int>> result1 = solution(no_of_switches, n - 2, reversed);
+	    sum = result1.first;
 
-		//--------------------- turn off toggle right switch to n-2 switches from right
-		cout << "zering a bit from left" << endl;
-		switches[no_of_switches - n] = 0;   state(switches, no_of_switches);
+	    //--------------------- turn off toggle right switch to n-2 switches from right
+	    cout << no_of_switches - n << "\t";
+	    sum++;
+
+	    //--------------------- turn on n-2 switches from right 
+	    pair<int, vector<int>> result2 = solution(no_of_switches, n - 2, !reversed);
+	    sum += result2.first;
+
+	    //---------------------- solve same problem for n-1 switches
+	    pair<int, vector<int>> result3 = solution(no_of_switches, n - 1, reversed);
+	    sum += result3.first;
+
+	    //----------------------returning min number and sequece---------------------------------------
+	    vector<int> vec;
+	    for (int i = 0; i < result1.second.size(); i++)
+	       vec.push_back(result1.second[i]);
+	    vec.push_back(no_of_switches - n);
+	    for (int i = 0; i < result2.second.size(); i++)
+   	       vec.push_back(result2.second[i]);
+	    for (int i = 0; i < result3.second.size(); i++)
+	       vec.push_back(result3.second[i]);
+	    return make_pair(sum, vec);
+	}
+	else{
+	    int sum = 0;
+	    pair<int, vector<int>> result1 = solution(no_of_switches, n - 1, reversed);
+	    sum = result1.first;
 		
-		//--------------------- turn on n-2 switches from right
-		solution(switches, no_of_switches, n - 2, !reversed);
+            pair<int, vector<int>> result2 =  solution(no_of_switches, n - 2, !reversed);
+            sum += result2.first;
+        
+	    cout << no_of_switches - n << "\t";
+	    sum++;
+	
+	    pair<int, vector<int>> result3 = solution(no_of_switches, n - 2, reversed);
+	    sum += result3.first;
 
-		//---------------------- solve same problem for n-1 switches
-		solution(switches, no_of_switches, n - 1, reversed);
-	}
-	else
-	{
-		solution(switches, no_of_switches, n - 1, reversed);
-
-		solution(switches, no_of_switches, n - 2, !reversed);
-
-		cout << "oning a bit from left" << endl;
-		switches[no_of_switches - n] = 1;   state(switches, no_of_switches);
-		
-		solution(switches, no_of_switches, n - 2, reversed);
+	    vector<int> vec;
+	    for (int i = 0; i < result1.second.size(); i++)
+    	      vec.push_back(result1.second[i]);
+    	    for (int i = 0; i < result2.second.size(); i++)
+    	      vec.push_back(result2.second[i]);
+    	    vec.push_back(no_of_switches - n);
+    	    for (int i = 0; i < result3.second.size(); i++)
+    	      vec.push_back(result3.second[i]);
+    	    return make_pair(sum, vec);
 	}
 }
 
 int main()
 {
-	int no_of_switches = 6;
-	bool* switches = new bool[no_of_switches];
-	for (int i = 0; i < no_of_switches; i++)
-	switches[i] = true;
-	state(switches, no_of_switches);
-	solution(switches, no_of_switches, no_of_switches, 0);
+    int no_of_switches = 5;
+    cout << "sequence : ";
+    pair<int, vector<int>> result = solution(no_of_switches, no_of_switches,0);
+    cout << "\nmin number = " << result.first << endl;
 }
